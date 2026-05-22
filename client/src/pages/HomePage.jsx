689482e1372af65f2ab63d1e7e96ecb6ebae94f4
Avo_Okas <rasmus.okas@gmail.com>
2026-05-22 20:31:50 +0300

    Avaleht tagasi originaali

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,10 +2,8 @@ import Leaderboard from "../components/Leaderboard"
 import Matches from "../components/Matches"
 import useLeaderboard from "../hooks/useLeaderboard"
 import useMatches from "../hooks/useMatches"
-import { useState } from "react"
 
 function HomePage() {
-  const [showAllMatches, setShowAllMatches] = useState(false)
   const {
     leaderboard,
     loading,
@@ -16,21 +14,7 @@ function HomePage() {
     loading: matchesLoading,
     error: matchesError
   } = useMatches()
-  const visibleMatches = showAllMatches
-    ? matches
-    : matches.filter(match => isToday(match.utcDate))
-  function isToday(date) {
-    if (!date) return false
 
-    const today = new Date()
-    const matchDate = new Date(date)
-
-    return (
-      today.getFullYear() === matchDate.getFullYear() &&
-      today.getMonth() === matchDate.getMonth() &&
-      today.getDate() === matchDate.getDate()
-    )
-  }
 
   return (
     <div className="container">
@@ -55,18 +39,10 @@ function HomePage() {
 
         {matchesError && <p>{matchesError}</p>}
 
-        <button
-          className="secondary-button"
-          onClick={() => setShowAllMatches(!showAllMatches)}
-        >
-          {showAllMatches ? "Näita ainult tänaseid mänge" : "Näita kõiki mänge"}
-        </button>
-
-        {visibleMatches.length === 0 && (
-          <p>Tänase kuupäeva mänge ei ole.</p>
+        {!matchesLoading && !matchesError && (
+          <Matches matches={matches} />
         )}
 
-        <Matches matches={visibleMatches} />
       </div>
     </div>
   )
